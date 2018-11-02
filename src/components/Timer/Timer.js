42e80277d12e33760d58a8984bf8deb5b351c6c6
Tenzin Kunkyab <ten.kunkyab@gmail.com>
2018-11-02 20:40:09 +0530

    Added Timer

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 export default class Timer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      remainder: 0
+    }
+    this.reset = this.reset.bind(this);
+    this.updateTimer = this.updateTimer.bind(this);
+  }
+
+  reset() {
+    this.setState({
+      remainder: 0
+    })
+  }
+
+  updateTimer(event) {
+    this.setState({
+      remainder: parseInt(event.target.value)/10
+    })
+  }
+
   render() {
+    let { remainder } = this.state;
     return (
       <div>
-        Timer
+        <div>
+          <label>Elapsed Time: </label>
+          <progress max={100} value={ remainder }></progress>
+        </div>
+        <label>{ remainder }s</label><br/>
+        <label>Duration</label>
+        <input type="range" onChange={ this.updateTimer } />
+        <br/>
+        <input type="button" onClick={ this.reset } value="reset" />
       </div>
     )
   }
