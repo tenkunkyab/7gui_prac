016c8a9b97b2a65ae34aed7a39e30de5e781e210
Tenzin Kunkyab <ten.kunkyab@gmail.com>
2019-04-06 23:53:01 +0530

    Added Timer Component and updated the CRA version to 2.x

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,41 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Timer extends Component {
   constructor() {
     super();
     this.state = {
-      remainder: 0
-    }
+      remainder: 0,
+      elapsed: 0
+    };
+    this.timer = 0;
     this.reset = this.reset.bind(this);
     this.updateTimer = this.updateTimer.bind(this);
   }
 
   reset() {
     this.setState({
-      remainder: 0
-    })
+      elapsed: 0
+    });
   }
 
   updateTimer(event) {
     this.setState({
-      remainder: parseInt(event.target.value)/10
-    })
+      remainder: Math.floor(parseInt(event.target.value) / 10)
+    });
   }
 
   render() {
-    let { remainder } = this.state;
+    let { remainder, elapsed } = this.state;
     return (
       <div>
         <div>
           <label>Elapsed Time: </label>
-          <progress max={100} value={ remainder }></progress>
+          <progress max={100} value={remainder} />
         </div>
-        <label>{ remainder }s</label><br/>
+        <label>{elapsed}s</label>
+        <br />
         <label>Duration</label>
-        <input type="range" onChange={ this.updateTimer } />
-        <br/>
-        <input type="button" onClick={ this.reset } value="reset" />
+        <input type="range" onChange={this.updateTimer} />
+        <label>{remainder}s</label>
+        <br />
+        <input type="button" onClick={this.reset} value="reset" />
       </div>
-    )
+    );
+  }
+
+  componentDidMount() {
+    let cmpt = this;
+    this.timer = setInterval(function() {
+      if (cmpt.state.remainder > cmpt.state.elapsed) {
+        cmpt.setState({
+          elapsed: cmpt.state.elapsed + 1
+        });
+      }
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
   }
 }
